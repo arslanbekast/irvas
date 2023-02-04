5df0c7f2bd7423217bcd41638bb777bd58b60bf9
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-02-04 18:09:35 +0300

    Implemented module while images

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
 import timer from './modules/timer';
+import images from './modules/images';
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
@@ -18,5 +19,6 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline');
     forms(modalState);
     timer('.container1', deadline);
+    images();
 
 });
